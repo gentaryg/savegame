e4bec24c7d1b7e3e97e5c8bdd6cc54f8c0c28b11
gentaryg <gentaryg@gmail.com>
2022-05-10 23:29:54 +0500

    modifi

--- src/GameProgress.java
+++ src/GameProgress.java
@@ -1,16 +1,13 @@
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.Serializable;
-import java.nio.charset.StandardCharsets;
+import java.io.*;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 public class GameProgress implements Serializable {
-    private static final long serialVersionUID = 1L;
-
     private int health;
     private int weapons;
     private int lvl;
     private double distance;
+    private File savegames = new File("/Users/a.shalygin/Games/savegames");
 
     public GameProgress(int health, int weapons, int lvl, double distance) {
         this.health = health;
@@ -29,12 +26,45 @@ public class GameProgress implements Serializable {
                 '}';
     }
 
-    public void saveGames(File gameProgress, GameProgress progress) {
-        try (FileOutputStream saveProgressWriter = new FileOutputStream(gameProgress)){
-            byte[] bytes = progress.toString().getBytes();
-            saveProgressWriter.write(bytes, 0, bytes.length);
-        } catch (IOException ex) {
-            ex.printStackTrace();
+
+    public void saveGame(String fileOutputStream, GameProgress gameProgress) {
+        try (FileOutputStream saveGame = new FileOutputStream(fileOutputStream);
+             ObjectOutputStream save = new ObjectOutputStream(saveGame)) {
+            save.writeObject(gameProgress);
+            System.out.println("Done!");
+        } catch (Exception e) {
+            System.out.println("Error!");
+        }
+    }
+
+    public void zipGame(String path) {
+        try (ZipOutputStream zout = new ZipOutputStream(new FileOutputStream(path))) {
+            for (File file : savegames.listFiles()) {
+                if (file.getName().endsWith(".zip")) continue;
+                try (FileInputStream fis = new FileInputStream(file)) {
+                    ZipEntry entry = new ZipEntry(file.getName());
+                    zout.putNextEntry(entry);
+                    byte[] buffer = new byte[fis.available()];
+                    fis.read(buffer);
+                    zout.write(buffer);
+                    zout.closeEntry();
+                    System.out.println("done fis");
+                } catch (IOException e) {
+                    System.out.println("error fis");
+                }
+            }
+            System.out.println("done zip");
+        } catch (IOException e) {
+            System.out.println("error zip");
+        }
+    }
+    public void del() {
+        for (File list : savegames.listFiles()) {
+            String name = list.getName();
+            String type = name.substring(name.lastIndexOf('.'));
+            if (type.equals(".txt")) {
+                list.delete();
+            }
         }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,56 +1,16 @@
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipOutputStream;
-
 public class Main {
 
     public static void main(String[] args) {
         GameProgress gameProgress1 = new GameProgress(100, 2, 10, 12.4);
         GameProgress gameProgress2 = new GameProgress(89, 4, 24, 123);
         GameProgress gameProgress3 = new GameProgress(10, 1, 30, 1234);
-        File savegames = new File("/Users/a.shalygin/Games/savegames");
-        try {
 
+        gameProgress1.saveGame("/Users/a.shalygin/Games/savegames", gameProgress1);
+        gameProgress2.saveGame("/Users/a.shalygin/Games/savegames", gameProgress2);
+        gameProgress3.saveGame("/Users/a.shalygin/Games/savegames", gameProgress3);
 
-            File progress1 = new File(savegames, "saveProgress1.txt");
-            progress1.createNewFile();
-            File progress2 = new File(savegames, "saveProgress2.txt");
-            progress2.createNewFile();
-            File progress3 = new File(savegames, "saveProgress3.txt");
-            progress3.createNewFile();
-            gameProgress1.saveGames(progress1, gameProgress1);
-            gameProgress2.saveGames(progress2, gameProgress2);
-            gameProgress3.saveGames(progress3, gameProgress3);
+        gameProgress1.zipGame("/Users/a.shalygin/Games/savegames/save.zip");
 
-
-            File zipSave = new File(savegames, "zip_save.zip");
-            ZipOutputStream zSave = new ZipOutputStream(new FileOutputStream(zipSave));
-            for (File saves : savegames.listFiles()) {
-                if (saves.getName().contains(".txt")) {
-                    FileInputStream saveProgressRead = new FileInputStream(saves);
-                    zSave.putNextEntry(new ZipEntry(saves.getName()));
-                    byte[] buffer = new byte[saveProgressRead.available()];
-                    saveProgressRead.read(buffer);
-                    zSave.write(buffer);
-                    saveProgressRead.close();
-                    zSave.closeEntry();
-                }
-            }
-
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
-        for (File item :savegames.listFiles()) {
-            if (item.getName().contains(".txt")) {
-                item.delete();
-            }
-        }
+        gameProgress1.del();
     }
-
-
-
-
 }
