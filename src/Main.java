291f7fc17ea4ca090e7eec63e6a5c63a9d6867af
gentaryg <gentaryg@gmail.com>
2022-05-12 18:17:45 +0500

    change

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,9 @@ public class Main {
         GameProgress gameProgress2 = new GameProgress(89, 4, 24, 123);
         GameProgress gameProgress3 = new GameProgress(10, 1, 30, 1234);
 
-        gameProgress1.saveGame("/Users/a.shalygin/Games/savegames", gameProgress1);
-        gameProgress2.saveGame("/Users/a.shalygin/Games/savegames", gameProgress2);
-        gameProgress3.saveGame("/Users/a.shalygin/Games/savegames", gameProgress3);
+        gameProgress1.saveGame("/Users/a.shalygin/Games/savegames/save1.txt", gameProgress1);
+        gameProgress2.saveGame("/Users/a.shalygin/Games/savegames/save2.txt", gameProgress2);
+        gameProgress3.saveGame("/Users/a.shalygin/Games/savegames/save3.txt", gameProgress3);
 
         gameProgress1.zipGame("/Users/a.shalygin/Games/savegames/save.zip");
 
